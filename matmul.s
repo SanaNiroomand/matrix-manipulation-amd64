7cd19173269187121374e4e724ff0a2c7997844f
Sana Niroomand <sana.niroomand@gmail.com>
2025-01-04 16:44:03 +0330

    adding comments

--- matmul.s
+++ matmul.s
@@ -88,59 +88,59 @@ read_matrix:
     ret
 
 multiply_matrices:
-    xor r8, r8
-.row_loop:
-    xor r9, r9
+    xor r8, r8 ; i = 0
+.row_loop: 
+    xor r9, r9 ; j = 0
 .column_loop:
-    xor r10, r10
+    xor r10, r10 ; k = 0
 .intersection_loop:
     mov r11, r8
-    imul r11, [n]
-    add r11, r10
+    imul r11, [n] ; row * columns
+    add r11, r10 ; + column
 
     mov r12, r10
-    imul r12, [q]
-    add r12, r9
+    imul r12, [q] ; row * columns
+    add r12, r9 ; + column
 
     mov r13, r8
-    imul r13, [q]
-    add r13, r9
+    imul r13, [q] ; row * columns
+    add r13, r9 ; + column
 
-    mov r14, [matA + r11 * 8]
-    imul r14, [matB + r12 * 8]
-    add [matC + r13 * 8], r14
+    mov r14, [matA + r11 * 8] ; A[i][k]
+    imul r14, [matB + r12 * 8] ; A[i][k] * B[k][j]
+    add [matC + r13 * 8], r14 ; add to the result
 
-    inc r10
+    inc r10 ; k++
     cmp r10, [n]
     jl .intersection_loop
 
-    inc r9
+    inc r9 ; j++
     cmp r9, [q]
     jl .column_loop
 
-    inc r8
+    inc r8 ; i++
     cmp r8, [m]
     jl .row_loop
 
     ret
 
 print_matrix:
-    xor r8, r8
+    xor r8, r8 ; i = 0
 .row_loop:
-    xor r9, r9
+    xor r9, r9 ; j = 0
 .column_loop:
     mov rax, r8
-    mul qword [q]
-    add rax, r9
-    shl rax, 3
+    mul qword [q] ; row * columns
+    add rax, r9 ; + column
+    shl rax, 3 ; 8 bytes for each number
 
-    push r8
+    push r8 ; push needed registers
     push r9
 
     mov rdi, element_out
     mov rsi, [matC + rax]
     xor rax, rax
-    call printf
+    call printf ; print C[i][j]
 
     pop r9
     pop r8
@@ -154,7 +154,7 @@ print_matrix:
 
     mov rdi, newline
     xor rax, rax
-    call printf
+    call printf ; print newline
 
     pop r9
     pop r8
